06fce76287647dd77900c35445c2a2237c4c27ca
MrZver <mrzver@gmail.com>
2013-10-11 12:50:16 +0300

    fix

--- app/tests.py
+++ app/tests.py
@@ -10,6 +10,8 @@ from app.models import User1
 
 
 class AppTest(TestCase):
+    fixtures = ['initial_data.json']
+
     def test_record_exist(self):
         mainuser = User1.objects.get(pk=1)
-        self.assertEqual(mainuser, True)
+        self.assertEqual(bool(mainuser), True)
